496594900a720ef423dc152a3dac1754b799b0be
jsotoca <jsotoca@unprg.edu.pe>
2021-02-19 22:47:55 -0500

    initial configutarion

---
+++ src/test/java/com/farmacia_spring/application/FarmaciaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia_spring.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
